3a7d5e4056dc9026414653ffeef4f2703b8f6468
jmgorton <jgorton23@gmail.com>
2022-06-04 16:49:46 -0500

    update websockb server - web socket testing

--- test.a/websockb/websockb.js
+++ test.a/websockb/websockb.js
@@ -17,24 +17,24 @@ const port = 9090;
 
 
 // // nginx example code...
-// console.log("Server started");
-// var Msg = '';
-// var WebSocketServer = websocket.Server
-//     , wss = new WebSocketServer({port: port}); // was 8010 in example
-//     wss.on('connection', function(ws) {
-//         ws.on('message', function(message) {
-//         console.log('Received from client: %s', message);
-//         ws.send('Server received from client: ' + message);
-//     });
-//  });
+console.log("Server started");
+var Msg = '';
+var WebSocketServer = websocket.Server
+    , wss = new WebSocketServer({port: port}); // was 8010 in example
+    wss.on('connection', function(ws) {
+        ws.on('message', function(message) {
+        console.log('Received from client: %s', message);
+        ws.send('Server received from client: ' + message);
+    });
+ });
 
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>hi from websockb! ' + os.hostname() + '</h1>\n' + '<a href="http://localhost:1313">Home</a>');
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<h1>hi from websockb! ' + os.hostname() + '</h1>\n' + '<a href="http://localhost:1313">Home</a>');
+// });
 
-server.listen(port, hostname, () => {
-    console.log(`Websockb server running at http://${hostname}:${port}/ :)`);
-});
+// server.listen(port, hostname, () => {
+//     console.log(`Websockb server running at http://${hostname}:${port}/ :)`);
+// });
